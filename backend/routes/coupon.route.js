8cbf7f41b3faba784b8f13d7ba9a30674ccedde7
jakearanas <22251753+moonfries@users.noreply.github.com>
2025-02-02 13:24:21 +0800

    Payment Routes

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
-import { getCoupon, validateCoupon } from "../controllers/coupon.controller";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getCoupons, validateCoupon } from "../controllers/coupon.controller.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, getCoupon)
+router.get("/", protectRoute, getCoupons)
 router.get("/validate", protectRoute, validateCoupon)
 
 export default router
